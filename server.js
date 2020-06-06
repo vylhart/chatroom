b09430108bd1b7ca0fab3aa6b8260ca3344bd0b2
Shashank Kumar <shashankgangwar12@gmail.com>
2020-06-06 23:39:49 +0530

    fixed index

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const PORT    = process.env.PORT || 5000
 app
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res)=> res.render(index))
+    .get('/', (req, res)=> res.render("index"))
     .listen(PORT, ()=>console.log('Listening...'))
