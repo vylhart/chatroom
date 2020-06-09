373dd5d433de37b9689b54247a2ad57532eb38f8
Shashank Kumar <shashankgangwar12@gmail.com>
2020-06-10 05:22:49 +0530

    test

--- client/element.js
+++ client/element.js
@@ -1,13 +1,34 @@
-const call    = document.getElementById('call')
-const receive = document.getElementById('receive')
-const join    = document.getElementById('join')
-const local   = document.getElementById('local')
-const remote1 = document.getElementById('remote1')
-const remote2 = document.getElementById('remote2')
+const call      = document.getElementById('call')
+const receive   = document.getElementById('receive')
+const access    = document.getElementById('access')
+const local     = document.getElementById('local')
+const div       = document.getElementById('videos')
 var localStream = null
+var peer        = []
+var conn        = false
+
+const config    = {
+    'iceServers': [
+        {'urls': 'stun:stun.stunprotocol.org:3478'},
+        {'urls': 'stun:stun.l.google.com:19302'},
+    ]
+}
+
+access.addEventListener('click', ()=>{
+    getLocalMedia()
+    if(localStream){
+        call.removeAttribute('disabled')
+        access.setAttribute('disabled')
+    }
+})
 
 call.addEventListener('click', ()=>{
-    console.log('Calling....')
-    
+    call.setAttribute('value', 'Calling...')
     reqCall()
 })
+
+setInterval(()=>{
+    if(conn){
+        reqCall()
+    }
+}, 5000)
--- client/media.js
+++ client/media.js
@@ -1,128 +1,103 @@
+function reqCall(){
+    socket.emit('reqCall')
+}
+
+socket.on('reqCall',(id)=>{
+    console.log('Receiving : '+ id)
+    receive.removeAttribute("disabled")
+    receive.setAttribute("value", "Receiving Call")
+    receive.addEventListener('click', ()=>{
+        receive.setAttribute("value", "Connecting")
+        socket.emit('resCall', id)
+        makePeerRemote(id)
+    })
+})
+
+socket.on('resCall', (id)=>{
+    if(peer[id]==null){
+        console.log('Connected : ' + id)
+        makePeerLocal(id)
+    }
+})
+
+
+socket.on('answer', (id, answer)=>{
+    if(peer[id]!=null){
+        peer[id].setRemoteDescription(new RTCSessionDescription(answer))
+    }
+})
+
+
+socket.on('offer',async function(id, offer){
+    if(peer[id]!=null){
+        await peer[id].setRemoteDescription(new RTCSessionDescription(offer))
+        const answer =await peer[id].createAnswer()
+        await peer[id].setLocalDescription(answer)
+        socket.emit('answer', id, answer)        
+    }
+})
+
+socket.on('ice', (id, ice)=>{
+        console.log('got ice');
+        peer[id].addIceCandidate(new RTCIceCandidate(ice))
+})
+
+
+function makePeer(id){
+    peer[id] = new RTCPeerConnection(config)
+    peer[id].addStream(localStream)
+    peer[id].ontrack = getRemoteStream
+    peer[id].onconnectionstatechange = onConnection
+}
+
+async function makePeerLocal(id){
+    makePeer(id)
+    const offer =await peer[id].createOffer()
+    await peer[id].setLocalDescription(offer)
+    socket.emit('offer', id, offer)
+
+    peer[id].onicecandidate =  (event)=>{
+        if(event.candidate){
+            socket.emit('ice', id, event.candidate)
+        }
+    }
+}
+
+function onConnection(event){
+    if(peer[id].connectionstate ==='connected'){
+        conn = true
+        console.log('Peer connected: '+ id)  
+    }
+    access.setAttribute('hidden', true)
+    call.setAttribute('hidden', true)
+    receive.setAttribute('value', 'Connected')
+
+
+}
+
+function getRemoteStream(event){
+    console.log('track');
+    var remote = document.createElement('video')
+    remote.setAttribute('autoplay', true)
+    remote.srcObject = event.streams[0]
+    div.appendChild(remote)
+}
+
 function getLocalMedia(){
     const constraints = {
         'video': true,
         'audio': false
     }
-
     navigator.mediaDevices.getUserMedia(constraints)
     .then(stream => {
         console.log('Got MediaStream:', stream)
         local.srcObject = stream
         localStream = stream
     })
-    .catch((e)=>showError(e))
-}
-getLocalMedia()
-
-const config = {
-    'iceServers': [
-        {'urls': 'stun:stun.stunprotocol.org:3478'},
-        {'urls': 'stun:stun.l.google.com:19302'},
-    ]
+    .catch(showError)
 }
 
 function showError(e){
     console.error('Error::', error)
 }
 
-var peer = []
-
-function reqCall(){
-    socket.emit('reqCall')
-}
-
-socket.on('resCall', (id)=>{
-    console.log('Connected : ' + id)
-    makePeerLocal(id)
-})
-
-socket.on('reqCall',async (id)=>{
-    console.log('Receiving : '+ id)
-    receive.removeAttribute("disabled")
-    receive.setAttribute("value", "Receiving")
-    receive.addEventListener('click', ()=>{
-        receive.setAttribute("value", "Connecting")
-        socket.emit('resCall', id)
-        
-        makePeerRemote(id)
-    })
-})
-
-
-socket.on('answer', (id, answer)=>{
-    if(peer[id]!=null){
-        const remoteDesc = new RTCSessionDescription(JSON.parse(answer))
-        peer[id].setRemoteDescription(remoteDesc)
-    }
-})
-
-
-async function makePeerLocal(id){
-    peer[id] = new RTCPeerConnection(config)
-    peer[id].ontrack = (event)=>{
-        console.log('track');
-        var div = document.getElementById('videos')
-        var remote = document.createElement('video')
-        remote.setAttribute('autoplay', true)
-        remote.srcObject = event.streams[0]
-        div.appendChild(remote)
-    }
-    peer[id].addStream(localStream)
-    const offer =await peer[id].createOffer()
-    await peer[id].setLocalDescription(offer)
-    socket.emit('offer', id, JSON.stringify(offer))
-
-    peer[id].onicecandidate =  (event)=>{
-        console.log('here');
-        
-        if(event.candidate){
-            console.log('also here');
-            
-            socket.emit('ice', id, event.candidate)
-        }
-    }
-
-    peer[id].addEventListener('connectionstatechange', (event=>{
-        if(peer[id].connectionstate ==='connected'){
-            console.log('Peer connected: '+ id)  
-        }
-    }))
-}
-
-
-
-socket.on('offer',async function(id, offer){
-    if(peer[id]!=null){
-        const remoteDesc = new RTCSessionDescription(JSON.parse(offer))
-        console.log(offer)
-        await peer[id].setRemoteDescription(remoteDesc)
-        console.log(peer[id].remoteDescription);
-        const answer =await peer[id].createAnswer()
-        await peer[id].setLocalDescription(answer)
-        socket.emit('answer', id, JSON.stringify(answer))        
-    }
-    else{
-        console.log('fuck');
-    }
-})
-
-function makePeerRemote(id){
-    peer[id] = new RTCPeerConnection(config)
-    peer[id].addStream(localStream)
-    peer[id].ontrack = (event)=>{
-        console.log('track');
-        
-        remote1.srcObject = event.streams[0]
-    }
-}
-
-socket.on('ice', (id, ice)=>{
-    try{
-        console.log('got ice');
-        
-        peer[id].addIceCandidate(new RTCIceCandidate(ice))
-    }
-    catch(e){
-        showError(e)
-    }
-})
